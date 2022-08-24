34facb83fdb68de26447dec4bbb770d4a61442c0
Sahil <ahmed.sahil60@gmail.com>
2022-08-24 22:05:05 +0530

    Finishing touches  and scroll left

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        youtube: ["Roboto"],
+      },
+    },
   },
   plugins: [],
 };
